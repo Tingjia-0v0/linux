0d5d7d0fa5d6eaa20fe5e74a87b84395b9b912d4
Tingjia <Tingjia@localhost>
2023-04-08 19:22:43 -0600

    strict load check when lb

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8184,7 +8184,9 @@ static int detach_tasks(struct lb_env *env)
 			 * scheduler fails to find a good waiting task to
 			 * migrate.
 			 */
-			if (shr_bound(load, env->sd->nr_balance_failed) > env->imbalance)
+			// if (shr_bound(load, env->sd->nr_balance_failed) > env->imbalance)
+			// 	goto next;
+			if (load > env->imbalance)
 				goto next;
 
 			env->imbalance -= load;
